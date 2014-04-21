5e07df53f686e48062a41302e64e0ef6de7c86cb
Lexxicon <dolorosul@gmail.com>
2014-04-21 17:14:55 -0500

    added empty tester classes

---
+++ webServices/honor-marmot-web-services/src/test/java/com/biotech/inudstries/web/services/account/impl/UserExtractorTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.biotech.inudstries.web.services.account.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created: Apr 21, 2014
+ * @author Brian Holman
+ *
+ */
+public class UserExtractorTest {
+
+	private static transient final Logger LOGGER = LoggerFactory.getLogger(UserExtractorTest.class);
+}
